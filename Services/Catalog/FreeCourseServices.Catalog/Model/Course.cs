086175df14bc5974b9f3a4e1788e25e03b074637
= <=>
2022-08-06 13:15:26 +0300

    Catolog Modeller eklendi.Response Dto oluşturuldu

---
+++ Services/Catalog/FreeCourseServices.Catalog/Model/Course.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourseServices.Catalog.Model
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string CourseDescription { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal CoursePrice { get; set; }
+        public string CourseUserId { get; set; }
+
+        public string CoursePicture { get; set; }
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CourseCreatedTime { get; set; }
+
+        public Feature Feature { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+
+        [BsonIgnore]
+        public Category Category { get; set; }
+
+    }
+}
---
+++ Services/Catalog/FreeCourseServices.Catalog/Model/Feature.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourseServices.Catalog.Model
+{
+    public class Feature
+    {
+        public int FeatureDuration { get; set; }
+    }
+}
