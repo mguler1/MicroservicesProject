bfe80104f9df51c63c6cc652ce381b7213098384
Muhammed <Muhammed@DESKTOP-RKEDQOI>
2023-02-19 21:14:16 +0300

    Payment servis oluşturuldu.

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -18,6 +18,7 @@ namespace FreeCourse.IdentityServer
             new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
             new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
             new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
+            new ApiResource("resource_payment"){Scopes={"payment_fullpermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
         public static IEnumerable<IdentityResource> IdentityResources =>
@@ -37,6 +38,7 @@ namespace FreeCourse.IdentityServer
                new ApiScope("basket_fullpermission","Basket Api için full erişim"),
                new ApiScope("discount_fullpermission","Discount Api için full erişim"),
                new ApiScope("order_fullpermission","Order Api için full erişim"),
+               new ApiScope("payment_fullpermission","Payment Api için full erişim"),
                new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
               
             };
@@ -59,7 +61,7 @@ namespace FreeCourse.IdentityServer
                    AllowOfflineAccess=true,
                    ClientSecrets={new Secret("secret".Sha256())},
                    AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
-                   AllowedScopes={"basket_fullpermission","discount_fullpermission,order_fullpermmission",IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,
+                   AllowedScopes={"basket_fullpermission","discount_fullpermission,order_fullpermmission,payment_fullpermission",IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,
                                   IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.StandardScopes.OfflineAccess,IdentityServerConstants.LocalApi.ScopeName,"roles"},
                   AccessTokenLifetime=1*60*60,
                   RefreshTokenExpiration=TokenExpiration.Absolute,
